a5f5999ff7218c38673cfa50b6abd3d5a4ba12a6
kehphin <yang.k@me.com>
2015-12-10 18:33:00 -0500

    v1.0

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+  $(document).ready(function () {
+  //your code here
+
   //sprites
   Crafty.sprite(15, 'images/snake.svg', {
     snake: [0, 0]  
@@ -23,6 +26,23 @@
  
   }); 
 
+  // MUSIC
+
+  Crafty.audio.add({
+    main: ["sounds/main.mp3"]
+  });
+
+  Crafty.audio.add({
+    win: ["sounds/win.wav"]
+  });
+
+  Crafty.audio.add({
+    lose: ["sounds/lose.wav"]
+  });
+
+  Crafty.audio.add({
+    eat: ["sounds/eat.wav"]
+  });
 
   COLORS = [
     'rgb(255, 0, 0)',
@@ -63,6 +83,16 @@
     return COLORS[rand];
   };
 
+var hiscores = [];
+$.ajax({
+  type:"GET",
+  dataType: "json",
+  crossDomain: true,
+  url:"http://snake-kyang.rhcloud.com/hiscores",
+  success: function(data){
+    hiscores = data;
+  }
+});
 
 
 /*
@@ -112,7 +142,6 @@
   var moveSnake = function(){
       var blocks = Crafty('block');
 
-      //console.log(blocks.length)
       var head = Crafty(blocks[0]);
       var last = Crafty(blocks[blocks.length - 1]);
 
@@ -249,6 +278,7 @@
     var first = Crafty(Crafty('snake')[0]);
     
     if (!feed.eaten && !feed.timeOuted && feed.hit('snakeHead')) {
+      Crafty.audio.play('eat');
       first.next_feedColor = feed.COLOR;
       
       STATISTIC[feed.COLOR] += 1;
@@ -258,6 +288,7 @@
       feed.destroy();
       
       generateFeed();
+
 
       createScore(SCORE += 1)
 
@@ -271,14 +302,52 @@
 //gameover
   var gameOver = function(){
     GAMEOVER = true;
+    Crafty.audio.play('lose');
 
     Crafty.e("2D, DOM, Text")
-          .attr({ w: 300, h: 20, x: 150, y: 200})
+          .attr({ w: 300, h: 20, x: 150, y: 80})
           .text("GAME OVER!")
           .css({ "text-align": "center", "color": "white", "font-size": "300%", "font-style": "bold", "font-family": "Impact"});
-       
-    prompt("please enter your name", "asdfasd");   
+    
+    if ((hiscores.length < 5) || (SCORE > hiscores[hiscores.length - 1].score)) {
+      var name = prompt("Congrats! You have reached the top 5 in Angry Snake! Please enter your name:");   
+      Crafty.audio.play('win');
+      $.ajax({
+        type:"POST",
+        dataType: "json",
+        crossDomain: true,
+        url:"http://snake-kyang.rhcloud.com/hiscores",
+        data: {
+          username: name,
+          score: parseInt(SCORE)
+        },
+        success: function(data){
+          hiscores = data;
+          renderScores(hiscores);
+        }
+      });
+    } else {
+      renderScores(hiscores);
+    }
  }
+
+var renderScores = function(hiscores) {
+  yheight = 230
+  Crafty.e("2D, DOM, Text")
+          .attr({ w: 300, h: 20, x: 150, y: 170})
+          .text("HIGH SCORES")
+          .css({ "text-align": "center", "color": "white", "font-size": "250%", "font-style": "bold", "font-family": "Impact"});
+    
+  for (var i=0; i<hiscores.length; i++) {
+    var player = hiscores[i];
+
+    Crafty.e("2D, DOM, Text")
+          .attr({ w: 300, h: 20, x: 150, y: yheight})
+          .text(i+1 + ". " + player.username + ": " + player.score)
+          .css({ "text-align": "center", "color": "white", "font-size": "150%", "font-style": "bold", "font-family": "Impact"});
+    yheight += 40;
+  }
+}
 
  var createScore = function(score) {
   var scores = Crafty('Score');
@@ -309,6 +378,8 @@ Crafty.scene('main', function(){
 
   //create feed
   generateFeed();
+
+  Crafty.audio.play('main', -1, 0.3)
 
   createScore(SCORE);
   
@@ -404,3 +475,122 @@ Crafty.scene('main', function(){
       }
     })
 })
+
+  //component block
+      Crafty.c("block", {
+          init: function(){
+            this.addComponent("2D,Canvas,Color,Tween, Collision, snake")
+                .attr({w: BLOCKSIZE, h: BLOCKSIZE});  
+          },
+          makeBlock: function(x, y, current_dir, next_dir, color){
+            this.attr({x: x, y: y, current_dir: current_dir, next_dir: next_dir, COLOR: color, current_feedColor: false, next_feedColor: false})
+                .color(color);
+            return this;
+          },
+          moveTo: function(){
+            if (this.hit('Border')) {
+              this.x = this.old_x;
+              this.y = this.old_y;
+              return this;
+            } else {
+              this.attr({old_x: this.x, old_y: this.y})
+                  .move(this.current_dir, BLOCKSIZE);
+              this.current_dir = this.next_dir; 
+              return this;
+            }
+          },
+          eating: function(){
+            if (this.current_feedColor){
+              var big = Crafty.e('2D, Canvas, Color, Tween, big')
+                              .attr({w: this.w + 8, h: this.h + 8, x: this.x - 4, y: this.y - 4})
+                              .color(this.COLOR)
+                              .tween({alpha: 0.0}, 20);
+              window.setTimeout(function(){big.destroy(); }, 200);
+              if (this.has('tail')){
+                Crafty.e('block, tail').makeBlock(this.x, this.y, "", this.current_dir, this.current_feedColor);
+                this.removeComponent('tail');
+              }
+            };
+              this.current_feedColor = this.next_feedColor;
+              return this;
+          },
+      });
+    //snake head
+      Crafty.c('snakeHead', {
+        init: function(){
+           this.requires('block')
+               .addComponent('Collision')
+               .removeComponent('snake')
+        },
+
+        addPic: function(pic) {
+           this.attr({pic: pic})
+               .addComponent(pic);
+        return this;
+        }
+/*
+        makeHead: function(x, y, current_dir, next_dir, color){
+          this.attr({x: x, y: y, current_dir: current_dir, next_dir: next_dir, COLOR: color})
+              .color(color);
+          return this;
+        },
+*/
+      });
+
+ //feed
+    Crafty.c('feed', {
+      init: function(){
+        this.addComponent('2D, Canvas, Color, Tween, Collision, egg')
+            .attr({w: BLOCKSIZE, h: BLOCKSIZE});
+      },
+
+      makeFeed: function(x, y, color, timeOut){
+        this.attr({x: x, y: y, COLOR: color, timeOut: timeOut, eaten: false, timeOuted: false})
+            .color(color);
+        return this;
+      },
+      
+      fadeOut: function(){
+            this.removeComponent('Collision')
+                .tween({alpha: 0.0}, 20);
+            return this;
+      },
+
+      setTimeOut: function(){
+            if (this.timeOut == 0 && !this.timeOuted) {
+              this.fadeOut();
+              this.timeOuted = true;
+              Crafty.e('2D, Canvas, Color, Tween, Gravity, Collision, Border')
+                    .attr({w: this.w, h: this.h, x: this.x, y: this.y})
+                    .color(this.COLOR)
+                    .gravity('2D')
+                    .color('white');
+              this.destroy();
+              generateFeed();
+            };
+      },
+
+
+    });
+ //achievements
+    Crafty.c('Achievement',{
+      init: function(){
+        this._image = Crafty.e('2D, DOM, Image, Tween').image('images/achievement.png').attr({x: 30, y: 30});
+        this._text = "Set the Text";
+        this._center = WIDTH / 2 - ((this._text.length * 15) / 2);
+        this._textElement = Crafty.e('2D, DOM, Text, Tween').text(this._text).textColor('#dacfca').attr({x: this._center, y: 110, w:400}).textFont({weight: 'bold', size: '20px', family: 'Arial'});
+
+        this._image.tween({alpha: 0.0}, 200);
+        this._textElement.tween({alpha: 0.0}, 200);
+      },
+
+      text: function(newText) {
+        this._text = newText;
+        this._center = WIDTH / 2 - ((this._text.length * 12) / 2) ;
+        this._textElement.attr({x: this._center}).text(this._text);
+      }
+     
+    });
+
+  });
+
